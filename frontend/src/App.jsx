948fea0fe94d6468cf9224e583749d2998a7d8c8
SRIJANA BHAKRI <srijanabhakri5845@gmail.com>
2026-08-11 23:41:03 +0545

    Bordwerrounter in main jsx so only Routes with routes is here in the App jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,43 @@
-import React from 'react'
-import Navbar from './components/Navbar'
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+
+// import Navbar from "./components/Navbar";
+// import Login from "./pages/Login";
+// import Register from "./pages/Register";
+import CitizenDashboard from "./pages/CitizenDashboard";
+import AdminDashboardLayout from "./pages/AdminDashboardLayout";
+
 
 function App() {
   return (
- <div className='min-h-screen bg-gray-50'>
+    <Routes>
+      {/* Redirect home route to login */}
+      {/* <Route path="/" element={<Navigate to="/login" />} /> */}
 
-  <Navbar/>
- </div>
-  )
+      {/* Authentication routes */}
+      {/* <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} /> */}
+
+      {/* Citizen Dashboard */}
+      <Route path="/citizen" element={<CitizenDashboard />} />
+      <Route path="/" element={<Navigate to="/citizen" />} />
+
+
+
+      {/* Admin dashboard */}
+
+     
+  {/* <Route path="/admin" element={<AdminDashboardLayout />} /> */}
+
+
+   {/* Redirect home to Admin Dashboard */}
+      {/* <Route path="/" element={<Navigate to="/admin" />} /> */}
+
+    </Routes>
+
+
+
+  );
 }
 
-export default App
+export default App;
